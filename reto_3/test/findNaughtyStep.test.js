c9934ab4cd7ee53ed68d9f1a1d597762f97318c6
LEO <5674931+leomalaveraG@users.noreply.github.com>
2023-12-07 14:17:32 -0500

    reto_3(tests):adding tests

---
+++ reto_3/test/findNaughtyStep.test.js
@@ -0,0 +1,27 @@
+const findNaughtyStep = require("../findNaughtyStep");
+
+describe('Naughty elf', () => {
+  describe('findNaughtyStep', () => {
+    test('empty sequences', () => {
+      expect(findNaughtyStep('', '')).toBe('');
+    });
+  
+    test('additional step at the end', () => {
+      expect(findNaughtyStep('abcd', 'abcde')).toBe('e');
+    });
+  
+    test('additional step in the middle', () => {
+      expect(findNaughtyStep('stepfor', 'stepor')).toBe('f');
+    });
+  
+    test('no difference', () => {
+      expect(findNaughtyStep('abcde', 'abcde')).toBe('');
+    });
+  
+    test('additional step in the middle with numbers', () => {
+      expect(findNaughtyStep('123', '12X3')).toBe('X');
+    });
+  
+    // Add more test cases as needed
+  });
+})
